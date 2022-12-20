f2149187bf112e577528a66d61987ce62d002f85
Young-Ho Kim <yghokim@gmail.com>
2022-12-21 04:23:14 +0900

    Hid CHI2023 info.

--- frontend/src/sections/PublicationSection.tsx
+++ frontend/src/sections/PublicationSection.tsx
@@ -7,9 +7,7 @@ import 'prism-themes/themes/prism-solarized-dark-atom.css'
 const BibTexO4R = "@inproceedings{wei2023llmchatbot,\n\
     title        = {{Leveraging Large Language Models to Power Chatbots for Collecting User Self-Reported Data}},\n\
     author       = {Wei, Jing and Kim, Sungdong and Jung, Hyunhoon and Kim, Young-Ho},\n\
-    year         = {2023},\n\
-    month        = {apr},\n\
-    booktitle    = {Proceedings of the 2023 CHI Conference on Human Factors in Computing Systems}\n\
+    year         = {2022},\n\
 }"
 
 export const PublicationSection = () => {
@@ -17,13 +15,12 @@ export const PublicationSection = () => {
     return <Section title={"Citing This Work"}>
         <p>
             The bootstrapping method of the self-reporting chatbots and a user study are described in
-            our research paper published at <a href={"https://chi2023.acm.org/"} target={"_blank"} rel="noreferrer"><b>ACM CHI 2023</b></a>.
+            our research paper:
         </p>
         <div className={"flex"}>
             <div className={"py-3 border-y-[1px] border-gray-100/20"}>
                 <div>Jing Wei, Sungdong Kim, Hyunhoon Jung, and Young-Ho Kim. 2023.</div>
                 <div className={"font-medium"}>Leveraging Large Language Models to Power Chatbots for Collecting User Self-Reported Data.</div>
-                <div>In Proceedings of The ACM CHI Conference on Human Factors in Computing Systems 2023 (CHI'23). To appear.</div>
             </div>
         </div>
 
