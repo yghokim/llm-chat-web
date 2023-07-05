86f2ff21bd3e5ff7f2b6fcc822be0560bba90e76
Young-Ho Kim <yghokim@younghokim.net>
2023-07-05 10:25:17 +0900

    Added Arxiv information to the webpage.

--- frontend/src/sections/PublicationSection.tsx
+++ frontend/src/sections/PublicationSection.tsx
@@ -5,9 +5,14 @@ import 'prism-themes/themes/prism-solarized-dark-atom.css'
 
 // eslint-disable-next-line no-multi-str
 const BibTexO4R = "@inproceedings{wei2023llmchatbot,\n\
-    title        = {{Leveraging Large Language Models to Power Chatbots for Collecting User Self-Reported Data}},\n\
-    author       = {Wei, Jing and Kim, Sungdong and Jung, Hyunhoon and Kim, Young-Ho},\n\
-    year         = {2022},\n\
+    title={Leveraging Large Language Models to Power Chatbots for Collecting User Self-Reported Data}, \n\
+    author={Jing Wei and Sungdong Kim and Hyunhoon Jung and Young-Ho Kim},\n\
+    year={2023},\n\
+    eprint={2301.05843},\n\
+    archivePrefix={arXiv},\n\
+    primaryClass={cs.HC},\n\
+    url={https://doi.org/10.48550/arXiv.2301.05843},\n\
+    doi={arXiv:2301.05843}\n\
 }"
 
 export const PublicationSection = () => {
@@ -21,6 +26,7 @@ export const PublicationSection = () => {
             <div className={"py-3 border-y-[1px] border-gray-100/20"}>
                 <div>Jing Wei, Sungdong Kim, Hyunhoon Jung, and Young-Ho Kim. 2023.</div>
                 <div className={"font-medium"}>Leveraging Large Language Models to Power Chatbots for Collecting User Self-Reported Data.</div>
+                <a className={"text-orange-300"} href={"https://arxiv.org/abs/2301.05843"} target={"_blank"} rel="noreferrer">https://arxiv.org/abs/2301.05843</a>
             </div>
         </div>
 
